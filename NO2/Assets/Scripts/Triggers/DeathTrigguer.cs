35e185a5c3c406b81de957d997697b230e2f18a8
Vthot2004 <65424747+Vthot2004@users.noreply.github.com>
2026-06-02 18:12:27 +0200

    DeathTrigger V1 made

---
+++ NO2/Assets/Scripts/Triggers/DeathTrigguer.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class DeathTrigguer : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Buscamos el SceneController y le pasamos nuestra posición
+            SceneController sc = FindAnyObjectByType<SceneController>();
+            if (sc != null)
+            {
+                sc.KillPlayer();
+
+                //esto es pa reproducir sonido
+                //sc.ReproducirCheckPoint(); 
+                Debug.Log("Muerte alcanzada " + gameObject.name);
+            }
+
+            // Desactivar el trigger para que no se use más de una vez
+            GetComponent<Collider2D>().enabled = false;
+        }
+    }
+}

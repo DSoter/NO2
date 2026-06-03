f89d5cb53315d8912146fe90753c0888f9546fa6
Vthot2004 <65424747+Vthot2004@users.noreply.github.com>
2026-06-03 19:16:10 +0200

    Menu priincipal hecho

---
+++ NO2/Assets/Scripts/Managers/MainMenuSceneController.cs
@@ -0,0 +1,69 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuSceneManager : MonoBehaviour
+{
+
+    public Button playButton;
+    public GameObject canvasCredits;
+    public GameObject canvasOptions;
+    [SerializeField] private AudioClip theme;
+
+
+    void Update()
+    {
+        if (canvasCredits.activeSelf || canvasOptions.activeSelf)
+        {
+            //if (Input.anyKeyDown)
+            //{
+            //    canvasCredits.SetActive(false);
+            //    canvasOptions.SetActive(false);
+
+            //}
+        }
+    }
+
+    void Start()
+    {
+        if (theme != null)
+        {
+            GameManager.Instance.audioManager.PlayMusic(theme);
+        }
+        //EventSystem.current.SetSelectedGameObject(defaultButton.gameObject);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+
+    public void OnEnterName(string playerName)
+    {
+        Debug.Log("PlayerName: " + playerName);
+    }
+
+    public void OnSFXVolume(float volume)
+    {
+        Debug.Log("Volumen de Efectos: " + volume);
+    }
+
+    public void OnMusicVolume(float volume)
+    {
+        Debug.Log("Volumen de Música: " + volume);
+    }
+
+}

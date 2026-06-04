f140a501cb9914725d83764d5c24ae241681691a
DSoter <davidgn.04@gmail.com>
2026-06-04 20:14:59 +0200

    Update Destroyable.cs

--- NO2/Assets/ScriptableObjects/C#/Destroyable.cs
+++ NO2/Assets/ScriptableObjects/C#/Destroyable.cs
@@ -3,11 +3,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Destroyable", menuName = "Scriptable Objects/Destroyable")]
 public class Destroyable : ScriptableObject
 {
+    public enum DestruibleType { 
+        BOX, 
+        BARRICADE 
+    }
+
     public string objectName;
     public Sprite sprite;
     public float maxHealth;
     public float health;
     public float amountOfMoney;
-    public enum DestruibleType { BOX, BARRICADE }
+   
     public DestruibleType type;
 }

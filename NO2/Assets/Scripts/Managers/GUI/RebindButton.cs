f678a48f555acd4186f277a98533f490757bcf69
Vthot2004 <65424747+Vthot2004@users.noreply.github.com>
2026-06-09 19:44:07 +0200

    Squashed commit of the following:
    commit 26a6568a7adde949d10ea79dae45037dc3f9ecef
    Author: Vthot2004 <65424747+Vthot2004@users.noreply.github.com>
    Date:   Tue Jun 9 19:43:40 2026 +0200
    
        Terminada interfaz grafica keybinds
    
    commit a463ef7933d24ca619d6fc8fc2b513ddb60e4c04
    Merge: bf11052 29300c2
    Author: Vthot2004 <65424747+Vthot2004@users.noreply.github.com>
    Date:   Tue Jun 9 18:24:42 2026 +0200
    
        Merge branch 'main' into keybinds-try
    
    commit bf11052fa4f8f26944db33d9f19b3e5a8ee318e9
    Author: Vthot2004 <65424747+Vthot2004@users.noreply.github.com>
    Date:   Tue Jun 9 18:24:14 2026 +0200
    
        MenuPrincipal quitao xd
    
    commit 87655a3cb79ab9f0f85f8d1eb2f2a7f4ef190a0a
    Author: Vthot2004 <65424747+Vthot2004@users.noreply.github.com>
    Date:   Tue Jun 9 18:21:04 2026 +0200
    
        Keybind button
    
    commit 3236304d86f614694517b44970c22a0b2a59e4d3
    Author: Vthot2004 <65424747+Vthot2004@users.noreply.github.com>
    Date:   Tue Jun 9 16:58:09 2026 +0200
    
        Change inputs entrys by buttons

---
+++ NO2/Assets/Scripts/Managers/GUI/RebindButton.cs
@@ -0,0 +1,64 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RebindButton : MonoBehaviour
+{
+    [SerializeField] private InputActionReference actionReference;
+    [SerializeField] private TMP_Text buttonText;
+    [SerializeField] private int indexInput;
+
+    private InputSystem m_Actions;
+    private InputSystem.PlayerActions m_Player;
+
+    private InputActionRebindingExtensions.RebindingOperation rebindingOperation;
+    private void Start()
+    {
+        string json = PlayerPrefs.GetString("rebinds", "");
+
+        if (!string.IsNullOrEmpty(json))
+        {
+            m_Actions.asset.LoadBindingOverridesFromJson(json);
+        }
+        buttonText.text = InputControlPath.ToHumanReadableString(
+                actionReference.action.bindings[indexInput].effectivePath,
+                InputControlPath.HumanReadableStringOptions.OmitDevice
+            );
+
+        if (actionReference is null ) { Debug.Log("Falta Action Reference"); }
+        if (buttonText is null) { Debug.Log("Falta TMP_Text"); }
+        
+    }
+    public void StartRebinding()
+    {
+        buttonText.text = "...";
+
+        actionReference.action.Disable();
+
+        rebindingOperation = actionReference.action
+            .PerformInteractiveRebinding(indexInput).WithControlsExcluding("<Keyboard>/escape")
+            .OnComplete(operation =>
+            {
+                operation.Dispose();
+
+                actionReference.action.Enable();
+
+                UpdateBindingText();
+            });
+
+        rebindingOperation.Start();
+    }
+
+    private void UpdateBindingText()
+    {
+        buttonText.text =
+            InputControlPath.ToHumanReadableString(
+                actionReference.action.bindings[indexInput].effectivePath,
+                InputControlPath.HumanReadableStringOptions.OmitDevice
+            );
+        PlayerPrefs.SetString(
+    "rebinds",
+    m_Actions.asset.SaveBindingOverridesAsJson()
+);
+    }
+}
--- NO2/Assets/Scripts/Managers/GUI/VolumeOptionsManager.cs
+++ NO2/Assets/Scripts/Managers/GUI/VolumeOptionsManager.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.Audio;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class VolumeOptionsManager : MonoBehaviour
 {
@@ -14,6 +15,11 @@ public class VolumeOptionsManager : MonoBehaviour
     public GameObject soundOn;
     public GameObject soundOff;
 
+
+    //private InputSystem m_Actions;
+    //private InputSystem.UIActions m_UI;
+    //private bool _wantsToExit;
+    //private bool _optionsOpened;
     void Start()
     {
         // Cargar valores guardados (o usar los de por defecto)
@@ -33,17 +39,28 @@ public class VolumeOptionsManager : MonoBehaviour
         musicSlider.onValueChanged.AddListener(SetMusicVolume);
         sfxSlider.onValueChanged.AddListener(SetSFXVolume);
     }
+    //private void Update()
+    //{
+    //    if (_wantsToExit && _optionsOpened)
+    //    {
+    //        _wantsToExit= false;
+    //        CloseOptions();
+    //    }
+    //}
 
     public void OpenOptions()
     {
         optionsPanel.SetActive(true);
+        //_optionsOpened= true;
     }
 
     public void CloseOptions()
     {
         optionsPanel.SetActive(false);
+        //_optionsOpened= false;
         PlayerPrefs.Save(); // guarda en disco
     }
+
 
 
     void SetMusicVolume(float value)
@@ -83,4 +100,22 @@ public class VolumeOptionsManager : MonoBehaviour
             soundOff.SetActive(false);
         }
     }
+
+
+    //public void OnEscape(InputAction.CallbackContext context)
+    //{
+    //    if (context.performed)
+    //    {
+    //        _wantsToExit = true;
+    //    }
+    //}
+    //void OnDestroy()
+    //{
+    //    m_Actions.Dispose();
+    //}
+    //void OnEnable()
+    //{
+    //    m_UI.Enable();
+    //}
+
 }

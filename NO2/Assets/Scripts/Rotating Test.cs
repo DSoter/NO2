a40b2a872000489a018fdb4b0f680bde10320891
DSoter <davidgn.04@gmail.com>
2026-06-21 23:04:15 +0200

    Weak Attack Basics

--- NO2/Assets/Scripts/Rotating Test.cs
+++ NO2/Assets/Scripts/Rotating Test.cs
@@ -1,35 +1,40 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
 
 public class RotatingTest : MonoBehaviour
 {
-    private Transform _center;
     private Animator _animator;
+    private SpriteRenderer _renderer;
 
     private float _attackCooldown = 0.5f;
     private float _attackTimer = 0;
+    private int _attackCounter = 0;
 
     private void Start()
     {
-        _center = transform.parent.transform;
         _animator = GetComponent<Animator>();
+        _renderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePos.z = 0;
 
-        var angle = Vector2.SignedAngle(mousePos - transform.position, transform.position + new Vector3(1, 0, 0) - transform.position);
-
-        Debug.Log(angle);
-
-        transform.rotation = Quaternion.Euler(0, 0, -angle);
 
         if (Input.GetButtonDown("Fire1") && _attackTimer <= 0)
         {
+            var mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            mousePos.z = 0;
+
+            var angle = Vector2.SignedAngle(mousePos - transform.position, transform.position + new Vector3(1, 0, 0) - transform.position);
+
+            transform.rotation = Quaternion.Euler(0, 0, -angle);
+
+            _renderer.flipY = _attackCounter % 2 == 1;
             _animator.SetTrigger("Play");
             _attackTimer = _attackCooldown;
+
+            _attackCounter += 1;
         }
 
         _attackTimer -= Time.deltaTime;

94ccf375203b8039f219b6439879e755a9bee8c6
Vthot2004 <65424747+Vthot2004@users.noreply.github.com>
2026-07-04 14:27:36 +0200

    Inventory_coins

---
+++ NO2/Assets/ScriptableObjects/_C#/MoneyData.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(fileName = "MoneyData", menuName = "Scriptable Objects/MoneyData")]
+public class MoneyData : ScriptableObject
+{
+    [SerializeField] private int money;
+    [SerializeField] private int maxAmount;
+
+    public event Action OnMoneyChanged;
+    public int Money
+    {
+        get { return money; }
+        set
+        {
+            money = Mathf.Clamp(value, 0, maxAmount);
+            OnMoneyChanged?.Invoke();
+            Debug.Log(money);
+        }
+    }
+
+}
--- NO2/Assets/Scripts/GUI/HUD/MoneyCounter.cs
+++ NO2/Assets/Scripts/GUI/HUD/MoneyCounter.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class MoneyCounter : MonoBehaviour
 {
-    [SerializeField] private PlayerData _playerData;
+    [SerializeField] private MoneyData _moneyData;
     [SerializeField] private TextMeshProUGUI counterText;
     private string displayText;
 
@@ -17,18 +17,45 @@ public class MoneyCounter : MonoBehaviour
 
     private void OnEnable()
     {
-        _playerData.OnOxygenChanged += UpdateCurrentMoney;
+        _moneyData.OnMoneyChanged += UpdateCurrentMoney;
     }
 
     private void OnDisable()
     {
-        _playerData.OnOxygenChanged -= UpdateCurrentMoney;
+        _moneyData.OnMoneyChanged -= UpdateCurrentMoney;
     }
 
     private void UpdateCurrentMoney()
     {
-        int numberCoins = (int)(_playerData.Oxygen);
-        displayText = "0000" + numberCoins as string;
+        int numberCoins = _moneyData.Money;
+        Debug.Log(_moneyData.Money);
+        switch (numberCoins)
+        {
+            case < 10:
+                displayText = "0000000" + numberCoins as string;
+                break;
+            case < 100:
+                displayText = "000000" + numberCoins as string;
+                break;
+            case < 1000:
+                displayText = "00000" + numberCoins as string;
+                break;
+            case < 10000:
+                displayText = "0000" + numberCoins as string;
+                break;
+            case < 100000:
+                displayText = "000" + numberCoins as string;
+                break;
+            case < 1000000:
+                displayText = "00" + numberCoins as string;
+                break;
+            case < 10000000:
+                displayText = "0" + numberCoins as string;
+                break;
+            case < 100000000:
+                displayText = "" + numberCoins as string;
+                break;
+        }
         counterText.text = displayText;
         
     }

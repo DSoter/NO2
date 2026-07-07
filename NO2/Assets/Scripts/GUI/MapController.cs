174eed388ab1f85296e4b89162fa569a65c077a3
Vthot2004 <65424747+Vthot2004@users.noreply.github.com>
2026-07-07 23:44:11 +0200

    hover limited

--- NO2/Assets/Scripts/GUI/MapController.cs
+++ NO2/Assets/Scripts/GUI/MapController.cs
@@ -2,11 +2,12 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.Tilemaps;
 
 public class MapController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IScrollHandler, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
     [SerializeField] private Camera mapCamera;
-    [SerializeField] private string mapCameraTag = "MapCamera";
+    private string mapCameraTag = "MapCamera";
     [SerializeField] private float minFov = 5f;
     [SerializeField] private float maxFov = 50f;
 
@@ -20,12 +21,28 @@ public class MapController : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
     private Vector3 initialPosition = Vector3.zero;
 
 
+    private Tilemap sceneTileMap;
+    private string tileMapTag = "MainTileMap";
+
+
     private bool isHovered = false;
 
     private float targetFov;
 
+    private int xMax;
+    private int yMax;
+    private int xMin;
+    private int yMin;
+
     void Start()
     {
+        if (sceneTileMap != null)
+        {
+            
+            sceneTileMap.CompressBounds();
+            CalculateBounds();
+
+        }
         if(mapCamera == null)
         {
             return;
@@ -33,6 +50,8 @@ public class MapController : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
         targetFov = mapCamera.orthographicSize;
         velocity = Vector3.zero;
         initialPosition = mapCamera.transform.position;
+
+
     }
 
     public void OnScroll(PointerEventData eventData)
@@ -65,6 +84,10 @@ public class MapController : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
         }
         //mapCamera.fieldOfView = Mathf.Lerp(mapCamera.fieldOfView, targetFov, Time.unscaledDeltaTime * 10f);
         mapCamera.orthographicSize = Mathf.Lerp(mapCamera.orthographicSize, targetFov, Time.unscaledDeltaTime * 10f);
+        float maxSizeByHeight = (yMax - yMin) / 2f;
+        float maxSizeByWidth = (xMax - xMin) / 2f / mapCamera.aspect;
+
+        mapCamera.orthographicSize = Mathf.Min(mapCamera.orthographicSize, Mathf.Min(maxSizeByHeight, maxSizeByWidth));
 
         if (isDragging || velocity == Vector3.zero) {
             return;
@@ -78,6 +101,7 @@ public class MapController : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
             velocity = Vector3.zero;
         }
 
+        FixCameraPosition();
     }
     private void Awake()
     {
@@ -85,6 +109,11 @@ public class MapController : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
         {
             mapCamera =FindCamera();
             if(mapCamera == null) { Debug.Log("No se ha encontrado la camara"); }
+        }
+        if (sceneTileMap == null)
+        {
+            sceneTileMap = FindTileMap();
+            if(sceneTileMap == null) { Debug.Log("No se ha encontrado el tilemap"); }
         }
         
     }
@@ -141,6 +170,26 @@ public class MapController : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
         }
         return null;
     }
+    private Tilemap FindTileMap()
+    {
+        Scene additiveScene = gameObject.scene; // la escena donde vive este script
+
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            Scene scene = SceneManager.GetSceneAt(i);
+            if (scene == additiveScene) continue; // saltamos la aditiva
+
+            foreach (GameObject root in scene.GetRootGameObjects())
+            {
+                Tilemap tm= root.GetComponentInChildren<Tilemap>();
+                if (tm != null && tm.CompareTag(tileMapTag))
+                {
+                    return tm;
+                }
+            }
+        }
+        return null;
+    }
 
 
 
@@ -151,5 +200,31 @@ public class MapController : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
             mapCamera.transform.position = initialPosition;
         }
         
+    }
+    private void CalculateBounds()
+    {
+        BoundsInt bounds = sceneTileMap.cellBounds;
+        xMin = bounds.xMin;
+        xMax = bounds.xMax;
+        yMin = bounds.yMin;
+        yMax = bounds.yMax;
+    }
+    private void FixCameraPosition()
+    {
+        if( mapCamera != null && sceneTileMap != null)
+        {
+
+
+            float xAct = mapCamera.transform.position.x;
+            float yAct = mapCamera.transform.position.y;
+            float xLimInf = xMin + mapCamera.orthographicSize * mapCamera.aspect;
+            float xLimSup = xMax - mapCamera.orthographicSize * mapCamera.aspect;
+            float yLimInf = yMin + mapCamera.orthographicSize;
+            float yLimSup = yMax - mapCamera.orthographicSize;
+
+            xAct = Mathf.Clamp(xAct, xLimInf, xLimSup);
+            yAct = Mathf.Clamp(yAct, yLimInf, yLimSup);
+            mapCamera.transform.position = new Vector3(xAct, yAct, mapCamera.transform.position.z);
+        }
     }
 }

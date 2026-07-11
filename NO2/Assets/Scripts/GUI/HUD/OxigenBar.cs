aff7e542f5591c986594fb823624013717910936
DSoter <davidgn.04@gmail.com>
2026-07-12 01:17:35 +0200

    Oxigeno se actualiza progresivamente

--- NO2/Assets/Scripts/GUI/HUD/OxigenBar.cs
+++ NO2/Assets/Scripts/GUI/HUD/OxigenBar.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,7 +9,11 @@ public class OxigenBar : MonoBehaviour
     [SerializeField] private PlayerData _playerData;
     [SerializeField] private Image _fill;
     [SerializeField] private TextMeshProUGUI _percentageTMP;
+    [SerializeField] private float _updateSpeed = 1f;
+    [SerializeField] private float _snapDifference = 0.5f;
+
     private Animator _percentageAnimator;
+    private float _shownPercentage;
 
     private void Awake()
     {
@@ -16,7 +21,8 @@ public class OxigenBar : MonoBehaviour
     }
     private void Start()
     {
-        UpdateCurrentOxigen();
+        _shownPercentage = _playerData.Oxygen / _playerData.MaxOxygen * 100;
+        RefreshUI(_shownPercentage);
     }
 
     private void OnEnable()
@@ -31,13 +37,31 @@ public class OxigenBar : MonoBehaviour
 
     private void UpdateCurrentOxigen()
     {
-        int percentage = (int)(_playerData.Oxygen / _playerData.MaxOxygen * 100);
-
-        _percentageAnimator.SetFloat("Percentage", percentage);
-
-        _percentageTMP.text = percentage.ToString() + "%";
-
-        _fill.fillAmount = _playerData.Oxygen / _playerData.MaxOxygen;
+        StopAllCoroutines();
+        StartCoroutine(AnimateToTarget(_playerData.Oxygen / _playerData.MaxOxygen * 100));
     }
 
+    private IEnumerator AnimateToTarget(float targetPercentage)
+    {
+        while (!Mathf.Approximately(_shownPercentage, targetPercentage))
+        {
+            _shownPercentage = Mathf.Lerp(_shownPercentage, targetPercentage, _updateSpeed * Time.deltaTime);
+
+            // Para que no se quede infinitamente cerca del objetivo, al estar suficientemente cerca forzamos a que se iguale
+            if (Mathf.Abs(_shownPercentage - targetPercentage) < _snapDifference)
+            {
+                _shownPercentage = targetPercentage;
+            }
+
+            RefreshUI(_shownPercentage);
+            yield return null;
+        }
+    }
+
+    private void RefreshUI(float percentage)
+    {
+        _percentageAnimator.SetFloat("Percentage", percentage);
+        _percentageTMP.text = (int)percentage + "%";
+        _fill.fillAmount = percentage / 100f;
+    }
 }

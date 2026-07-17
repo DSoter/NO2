7ad9d68e00beb3f8ae1749c3671be7fd0c1fcaf5
Vthot2004 <65424747+Vthot2004@users.noreply.github.com>
2026-07-17 23:54:16 +0200

    animacion fog

--- NO2/Assets/Scripts/GUI/Map/FogTrigger.cs
+++ NO2/Assets/Scripts/GUI/Map/FogTrigger.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class FogTrigger : MonoBehaviour
@@ -10,8 +11,26 @@ public class FogTrigger : MonoBehaviour
 
         if (collision.CompareTag("Player"))
         {
-            Destroy(gameObject);
+
+            Destroy(fogCollider);
+            StartCoroutine(ShrinkAndDestroy());
 
         }
+    }
+
+    private IEnumerator ShrinkAndDestroy()
+    {
+        Vector3 initialScale = transform.localScale;
+        float t = 0f;
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime;
+            Vector3 newVector = Vector3.Lerp(initialScale, Vector3.zero, t);
+            transform.localScale = new Vector3 (newVector.x, newVector.y, 1);
+            yield return null;
+        }
+
+        Destroy(gameObject);
     }
 }

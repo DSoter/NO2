889d549433341d79c4f3265fad6b9c457bb1eb8c
DSoter <davidgn.04@gmail.com>
2026-08-12 14:14:23 +0200

    Rock-Destructibles

--- NO2/Assets/Scripts/World/Hitables/StrongDestructible.cs
+++ NO2/Assets/Scripts/World/Hitables/StrongDestructible.cs
@@ -1,9 +1,8 @@
 using System;
+using Unity.Cinemachine;
 using UnityEngine;
 
-
-
-public class WeakDestructible : MonoBehaviour, IHitable
+public class StrongDestructible : MonoBehaviour, IHitable
 {
     [Header("Content")]
     [SerializeField] private GameObject[] _content;
@@ -11,18 +10,55 @@ public class WeakDestructible : MonoBehaviour, IHitable
     [SerializeField] private float _minForce = 2f;
     [SerializeField] private float _maxForce = 6f;
 
-
     [Space(5)]
     [Header("Audio")]
     [SerializeField] private AudioClip _onDestructClip;
-    [SerializeField] [Range(0,1)] private float _volume = 1f;
-    [SerializeField] private float _pitchVariation = 0.3f;
-    
+    [SerializeField][Range(0, 1)] private float _destructVolume = 1f;
+    [SerializeField] private float _destructPitchVariation = 0.3f;
+
+    [Space(5)]
+    [SerializeField] private AudioClip _onHitClip;
+    [SerializeField][Range(0, 1)] private float _onHitVolume = 1f;
+    [SerializeField] private float _onHitPitchVariation = 0.3f;
+
+    [Space(5)]
+    [Header("Hit")]
+    [SerializeField] private int _hitPoints = 3;
+
+    [Space(5)]
+    [Header("Screen Shake")]
+    [SerializeField] private CinemachineImpulseSource _hitImpulse;
+    [SerializeField] private CinemachineImpulseSource _destructImpulse;
 
     public void Hit(Vector2 direction, float damage, AttackStrength strength)
     {
-        GameManager.Instance.audioManager.PlaySound(_onDestructClip, _volume, _pitchVariation);
+        switch (strength)
+        {
+            case AttackStrength.Weak:
+                _hitPoints--;
+                break;
+
+            case AttackStrength.Strong:
+                _hitPoints = 0;
+                break;
+        }
+
+        if (_hitPoints <= 0)
+        {
+            Destruct(direction);
+        }
+        else
+        {
+            GameManager.Instance.audioManager.PlaySound(_onHitClip, _onHitVolume, _onHitPitchVariation);
+            _hitImpulse.GenerateImpulse();
+        }
+    }
+
+    public void Destruct(Vector2 direction) {
+
+        GameManager.Instance.audioManager.PlaySound(_onDestructClip, _destructVolume, _destructPitchVariation);
         SpawnContent(direction);
+        _destructImpulse.GenerateImpulse();
 
         PersistentRespawnObject pro = gameObject.GetComponent<PersistentRespawnObject>();
         if (pro != null)
@@ -31,8 +67,6 @@ public class WeakDestructible : MonoBehaviour, IHitable
         }
 
         Destroy(gameObject);
-
-        GameManager.Instance.GetComponent<AchievementManager>().NotifyCounter("box_slayer", 1);
     }
 
     private void SpawnContent(Vector2 direction)
@@ -64,5 +98,5 @@ public class WeakDestructible : MonoBehaviour, IHitable
         return noisyDirection * UnityEngine.Random.Range(minForce, maxForce);
     }
 
-    
+
 }

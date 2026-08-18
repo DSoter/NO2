d5dde5d09fb50f084034ad6799ca0995202d81d7
Vthot2004 <65424747+Vthot2004@users.noreply.github.com>
2026-08-18 11:38:29 +0200

    dalogo mas pequeñoto

--- NO2/Assets/Scripts/Managers/GameManagerScripts/GUIOptionsReminder.cs
+++ NO2/Assets/Scripts/Managers/GameManagerScripts/GUIOptionsReminder.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class GUIOptionsReminder : MonoBehaviour
 {
     private int lastNotebookOption;
-    private int lastMenuOpen; //esto es para lo del menu de las flores y todo el rollo
 
     private void Awake()
     {
